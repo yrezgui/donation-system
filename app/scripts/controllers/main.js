b1a45de4ec3bc4cb0ad23c7dcce249262efd2273
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-03 17:40:32 +0200

    Initial commit
    Add of default files generated by Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('privataApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
