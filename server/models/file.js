89a641dfd41107bc085031aaf4b1fa43f8206a1a
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-05 23:49:59 +0200

    Add file model

---
+++ server/models/file.js
@@ -0,0 +1,39 @@
+// MongoDB ORM package
+var mongoose	= require('mongoose'),
+	Schema		= mongoose.Schema;
+
+// Database client
+var db = require('../db.js');
+
+var fileSchema = Schema({
+	fileId: {
+		type: String,
+		required: true
+	},
+	bucket: {
+		type: String,
+		required: true
+	},
+	size: {
+		type: Number,
+		required: true
+	},
+	icon: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	tags: [{
+		type: String
+	}]
+});
+
+// export the file model
+module.exports = db.model('File', fileSchema);
