be88a255a3d8a94bf61bb19196f9c47c510ae92a
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-05 23:05:00 +0200

    Add new config parameters
    - Express port
    - MongoDB URI connexion
    - Cookie secret
    - Cookie MaxAge
    - Amazon credentials

--- server/config.js
+++ server/config.js
@@ -1,13 +1,31 @@
 var config = require('config-env').define('NODE_ENV', function (config) {
 	config.common({
-		name: 'flouss'
+		name: 'flouss',
+		// Express port
+		express_port: process.env.PORT || 3000,
 	});
 
 	config.config('development', {
-		mongo_uri : 'mongodb://localhost/flouss'
+		// MongoDB connexion
+		mongo_uri: 'mongodb://localhost/flouss',
+		// Cookie secret
+		cookie_secret: 'YOLO',
+		// Cookie MaxAge
+		cookie_maxage: 2 * 60 * 60 * 1000,
+		// Amazon Credentials
+		aws_accessKeyId: 'AAAAAAAAAAAAAAAAAAA',
+		aws_secretAccessKey: 'XXXXXXXXXXXXXXXX'
 	});
 
 	config.config('production', {
-		mongo_uri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
+		// MongoDB connexion
+		mongo_uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL,
+		// Cookie secret
+		cookie_secret: '{eb+uq44KSvf.YM5+{7E1/;ZYfH)U3?wJcRPTY+6a}7j8+}F,_)})5M_~b?z5KC,',
+		// Cookie MaxAge
+		cookie_maxage: 60 * 60 * 1000,
+		// Amazon Credentials
+		aws_accessKeyId: 'AAAAAAAAAAAAAAAAAAA',
+		aws_secretAccessKey: 'XXXXXXXXXXXXXXXX'
 	});
 });
