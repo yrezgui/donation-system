b833b90f200be475fb9691c6340f44e953608228
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-08 21:44:34 +0200

    Update AngularJS module name

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('privataApp', [])
+angular.module('floussApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('privataApp')
+angular.module('floussApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
