d480086738945e908a3f8eb8d2f843a8c178d9a2
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-09 17:27:22 +0200

    Add home and login controllers (generated by Yeoman)

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('floussApp')
+	.controller('HomeCtrl', function ($scope) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+	});
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('floussApp')
+	.controller('LoginCtrl', function ($scope, flash) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+	});
