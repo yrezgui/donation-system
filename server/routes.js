98545cfa7b742893b6151514aadaa01110f4f9f4
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-09 08:35:41 +0200

    Add more API routes (File, Invoice)

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,6 @@
-var client = require('./controllers/api/client.js');
+var client	= require('./controllers/api/client.js');
+var file	= require('./controllers/api/file.js');
+var invoice	= require('./controllers/api/invoice.js');
 
 // Application back end routing
 module.exports = function(parent){
@@ -9,4 +11,18 @@ module.exports = function(parent){
 	parent.post('/api/client/', client.create);
 	parent.put('/api/client/:id', client.save);
 	parent.delete('/api/client/:id', client.remove);
+
+	// File entity API
+	parent.get('/api/file/', file.query);
+	parent.get('/api/file/:id', file.get);
+	parent.post('/api/file/', file.create);
+	parent.put('/api/file/:id', file.save);
+	parent.delete('/api/file/:id', file.remove);
+
+	// Invoice entity API
+	parent.get('/api/invoice/', invoice.query);
+	parent.get('/api/invoice/:id', invoice.get);
+	parent.post('/api/invoice/', invoice.create);
+	parent.put('/api/invoice/:id', invoice.save);
+	parent.delete('/api/invoice/:id', invoice.remove);
 };
