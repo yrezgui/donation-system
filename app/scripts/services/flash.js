43bb57a29d9e4d411beb299eea7522869f89fc9d
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-09 14:27:04 +0200

    Flash service - Fix access to inexistant index
    Modify the type of messages storage

--- app/scripts/services/flash.js
+++ app/scripts/services/flash.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('floussApp')
-	.factory('flash', ['$scope', function ($scope) {
+	.factory('flash', ['$rootScope', function ($rootScope) {
 		// Storage of the messages
-		var currentFlashMessages	= [];
-		var nextFlashMessages		= [];
+		var currentFlashMessages	= {};
+		var nextFlashMessages		= {};
 
 		// Public API here
 		var instance = {
@@ -14,33 +14,45 @@ angular.module('floussApp')
 			},
 			// Remove a flash message for the next page
 			remove: function(index) {
-				delete nextFlashMessages[index];
+				if(nextFlashMessages[index]) {
+					delete nextFlashMessages[index];
+				}
 			},
 			// Get a flash message for the current page
 			get: function(index) {
-				return currentFlashMessages[index];
+				if(currentFlashMessages[index]) {
+					return currentFlashMessages[index];
+				}
 			},
 			// Get all flash messages for the current page
 			getAll: function() {
 				return currentFlashMessages;
 			},
+			// Get all flash messages for the next page
+			getAllNext: function() {
+				return nextFlashMessages;
+			},
 			// Remove all flash messages for the next page
 			flush: function(index, msg) {
-				nextFlashMessages = [];
+				nextFlashMessages = {};
 			},
 			// Add one more page lifetime for a flash message
 			renew: function(index) {
-				nextFlashMessages[index] = currentFlashMessages[index];
+				if(currentFlashMessages[index]) {
+					nextFlashMessages[index] = currentFlashMessages[index];
+				}
 			},
 			// Update the current flash messages for the new page, just loaded
 			changePage: function() {
+				var self = this;
+
 				currentFlashMessages = nextFlashMessages;
-				nextFlashMessages = [];
+				self.flush();
 			}
 		};
 
 		// Update the current flash messages with the old "next flash messages"
-		$scope.$on('$routeChangeSuccess', function (scope, next, current) {
+		$rootScope.$on('$routeChangeSuccess', function (scope, next, current) {
 			instance.changePage();
 		});
 
