b2eb3ce00ac35f8d60851afe738372e147653914
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-09 17:25:33 +0200

    Update the mainCtrl to be the controller of the all page (root controller)
    It updates the flash messages each time the route is changed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('floussApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope, flash) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+		$scope.alerts = flash.getAll();
+		
+		$scope.$on('$routeChangeSuccess', function () {
+			$scope.alerts = flash.getAll();
+		});
+	});
