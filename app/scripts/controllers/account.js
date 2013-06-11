0c88b6fd677479197f7414c38e6c12fe2d202a76
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-11 22:24:49 +0200

    Rename Home to Account front end controller

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('floussApp')
-	.controller('HomeCtrl', ['$scope', function HomeCtrl($scope) {
+	.controller('AccountCtrl', ['$scope', function AccountCtrl($scope) {
 		$scope.awesomeThings = [
 			'HTML5 Boilerplate',
 			'AngularJS',
