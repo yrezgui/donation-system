126632efe4593bf40d637a0bac073a2dd783bb5f
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-11 22:27:33 +0200

    Add page front end controller
    Add account view (blank file for now)

---
+++ app/scripts/controllers/Page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('floussApp')
+  .controller('PageCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
