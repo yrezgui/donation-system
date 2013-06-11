2bae79e1abed7637494c9ff03fa274d5dff47186
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-11 16:52:11 +0200

    Add authentification front end service

---
+++ app/scripts/services/auth.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('floussApp')
+	.factory('auth', ['$cookieStore', function auth($cookieStore) {
+		var user = null;
+		// It sets the current logged user with the info
+		user = $cookieStore.get('user');
+
+		// Public API here
+		return {
+			// Called when the user log in
+			login: function login(user) {
+				user = user;
+				$cookieStore.put('user', user);
+			},
+			// Return the current logged user
+			getUser: function getUser() {
+				return user;
+			},
+			// Remove the user cookie and set to null the current user object
+			logout: function logout() {
+				$cookieStore.remove('user');
+				user = null;
+			}
+		};
+	}]);
