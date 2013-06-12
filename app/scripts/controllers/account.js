a05797cc1615e7b14741029cb4cd8cb4aa720b50
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-12 06:59:07 +0200

    Add request to get the current connected client (account controller)

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('floussApp')
-	.controller('AccountCtrl', ['$scope', function AccountCtrl($scope) {
-		$scope.awesomeThings = [
-			'HTML5 Boilerplate',
-			'AngularJS',
-			'Karma'
-		];
+	.controller('AccountCtrl', ['$scope', 'Client', 'auth', function AccountCtrl($scope, Client, auth) {
+		console.log(auth.getUser());
+		
+		Client.get({id: auth.getId()},
+			function success(client) {
+
+				$scope.client = client;
+			},
+
+			function error(response) {
+				console.log('error', response);
+
+				//$scope.$emit('auth:loginRequired');
+			}
+		);
 	}]);
