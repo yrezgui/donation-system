31ccd836feadfcf536033c6ab687129de38f8c5c
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-12 09:49:01 +0200

    Update account front controller
    Fix changing the value of inputs
    Verify if the client is modified before send put request

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -3,5 +3,18 @@
 angular.module('floussApp')
 	.controller('AccountCtrl', ['$scope', 'auth', 'Restangular', function AccountCtrl($scope, auth, Restangular) {
 		
-		$scope.client = Restangular.one('client', auth.getId()).get();
+		var original = null;
+
+		Restangular.one('client', auth.getId()).get().then(function(resource) {
+			original = resource;
+			$scope.client = Restangular.copy(original);
+		});
+
+		$scope.isClean = function isClean() {
+			return angular.equals(original, $scope.client);
+		};
+
+		$scope.processEdit = function processEdit() {
+			$scope.client.put();
+		};
 	}]);
