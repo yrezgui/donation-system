4481ad641bd6dbc5dbda57447f12d893e31c692c
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-12 23:53:08 +0200

    Managing of unlogged user in the account page

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -5,16 +5,24 @@ angular.module('floussApp')
 		
 		var original = null;
 
-		Restangular.one('client', auth.getId()).get().then(function(resource) {
-			original = resource;
-			$scope.client = Restangular.copy(original);
-		});
+		Restangular.one('client', auth.getId()).get().then(
+			function success(resource) {
+				original = resource;
+				$scope.client = Restangular.copy(original);
+			},
+
+			function error() {
+				$scope.$emit('auth:loginRequired');
+			}
+		);
 
 		$scope.isClean = function isClean() {
 			return angular.equals(original, $scope.client);
 		};
 
 		$scope.processEdit = function processEdit() {
-			$scope.client.put();
+			$scope.client.put().then(function success(resource) {
+				auth.setUser(resource);
+			});
 		};
 	}]);
