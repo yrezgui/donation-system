79eb0270d92eb1a10204d19cc4b2ac96ee8c5e0d
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-14 00:11:03 +0200

    Rename File to Ebook routes

--- server/routes.js
+++ server/routes.js
@@ -1,30 +1,30 @@
 var client	= require('./controllers/api/client.js');
-var file	= require('./controllers/api/file.js');
+var ebook	= require('./controllers/api/ebook.js');
 var invoice	= require('./controllers/api/invoice.js');
 
 // Application back end routing
 module.exports = function(parent){
 
 	// Client entity API
-	parent.get('/api/client/', client.query);
+	parent.get('/api/client', client.query);
+	parent.post('/api/client', client.create);
 	parent.get('/api/client/:id', client.get);
-	parent.post('/api/client/', client.create);
 	parent.put('/api/client/:id', client.save);
 	parent.delete('/api/client/:id', client.remove);
 
 	parent.post('/api/client/login', client.login);
 
-	// File entity API
-	parent.get('/api/file/', file.query);
-	parent.get('/api/file/:id', file.get);
-	parent.post('/api/file/', file.create);
-	parent.put('/api/file/:id', file.save);
-	parent.delete('/api/file/:id', file.remove);
+	// Ebook entity API
+	parent.get('/api/ebook', ebook.query);
+	parent.post('/api/ebook', ebook.create);
+	parent.get('/api/ebook/:id', ebook.get);
+	parent.put('/api/ebook/:id', ebook.save);
+	parent.delete('/api/ebook/:id', ebook.remove);
 
 	// Invoice entity API
-	parent.get('/api/invoice/', invoice.query);
+	parent.get('/api/invoice', invoice.query);
+	parent.post('/api/invoice', invoice.create);
 	parent.get('/api/invoice/:id', invoice.get);
-	parent.post('/api/invoice/', invoice.create);
 	parent.put('/api/invoice/:id', invoice.save);
 	parent.delete('/api/invoice/:id', invoice.remove);
 };
