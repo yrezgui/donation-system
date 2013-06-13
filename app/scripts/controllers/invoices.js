d979614e488b243e52bed6fdff94d03b0e21d278
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-13 14:38:05 +0200

    First draft of invoicesCtrl (all invoices of the logged client)
    Add fake data for the view (it's temporary)

--- app/scripts/controllers/invoices.js
+++ app/scripts/controllers/invoices.js
@@ -3,5 +3,5 @@
 angular.module('floussApp')
 	.controller('InvoicesCtrl', ['$scope', 'auth', 'Restangular', function InvoicesCtrl($scope, auth, Restangular) {
 		
-		$scope.client = auth.getUser();
+		$scope.ebooks = Restangular.all('invoices').getList();
 	}]);
