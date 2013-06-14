b96eda6620ed63e81a2b12d64ff74ba56ac7c723
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-14 17:38:23 +0200

    First draft of the ebook detail page
    Add some CSS rules

--- app/scripts/controllers/ebookDetail.js
+++ app/scripts/controllers/ebookDetail.js
@@ -2,5 +2,9 @@
 
 angular.module('floussApp')
 	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', function EbookDetailCtrl($scope, Restangular, $routeParams) {
-		$scope.ebook = Restangular.one('ebook', $routeParams.id);
+		$scope.ebook = Restangular.one('ebook', $routeParams.id).get();
+
+		$scope.isQuantityValid = function isQuantityValid() {
+			return parseInt($scope.quantity) > 0;
+		};
 	}]);
