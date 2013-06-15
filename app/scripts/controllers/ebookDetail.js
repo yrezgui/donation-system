c424a2acc25d766850aac9646d4921fc40303419
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-15 21:24:43 +0200

    Complete ebookDetail page (just need the redirect the client to invoice)

--- app/scripts/controllers/ebookDetail.js
+++ app/scripts/controllers/ebookDetail.js
@@ -2,9 +2,29 @@
 
 angular.module('floussApp')
 	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', function EbookDetailCtrl($scope, Restangular, $routeParams) {
-		$scope.ebook = Restangular.one('ebook', $routeParams.id).get();
 
-		$scope.isQuantityValid = function isQuantityValid() {
-			return parseInt($scope.quantity) > 0;
+		Restangular.one('ebook', $routeParams.id).get().then(
+			function success(resource) {
+
+				$scope.ebook = resource;
+
+				$scope.spriteSettings = {
+					price: $scope.ebook.price,
+					name: $scope.ebook.title,
+					description: $scope.ebook.description
+				};
+			},
+
+			function error() {
+				$scope.$emit('page:notFound');
+			}
+		);
+
+		$scope.makePayment = function makePayment(token) {
+			console.log(token);
+			$scope.$digest();
+			$scope.wait = true;
 		};
+
+		$scope.wait = false;
 	}]);
