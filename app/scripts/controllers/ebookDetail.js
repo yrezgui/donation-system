51a592244c5d168de579310bb0af6d64ce46ec44
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-16 00:15:20 +0200

    Update ebookDetail front end controller to manage payments

--- app/scripts/controllers/ebookDetail.js
+++ app/scripts/controllers/ebookDetail.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('floussApp')
-	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', function EbookDetailCtrl($scope, Restangular, $routeParams) {
+	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', 'auth', 'flash', '$route', function EbookDetailCtrl($scope, Restangular, $routeParams, auth, flash, $route) {
 
 		Restangular.one('ebook', $routeParams.id).get().then(
 			function success(resource) {
@@ -21,9 +21,52 @@ angular.module('floussApp')
 		);
 
 		$scope.makePayment = function makePayment(token) {
-			console.log(token);
-			$scope.$digest();
 			$scope.wait = true;
+			$scope.$digest();
+
+			console.log(token);
+
+			var newInvoice = {
+				stripe_token: token.id,
+				client: auth.getId(),
+				file: $routeParams.id
+			};
+
+			Restangular.all('invoice').post(newInvoice).then(
+				function success(resource) {
+					console.log("Object saved OK", resource);
+				},
+
+				function error() {
+					console.log("There was an error saving");
+
+					flash.add('payment-error', {
+						type: 'error',
+						title: 'Error !',
+						content: 'The payment failed. Please enter correct information and retry.'
+					});
+
+					$route.reload();
+				}
+			);
+
+
+			Restangular.one('ebook', $routeParams.id).get().then(
+				function success(resource) {
+
+					$scope.ebook = resource;
+
+					$scope.spriteSettings = {
+						price: $scope.ebook.price,
+						name: $scope.ebook.title,
+						description: $scope.ebook.description
+					};
+				},
+
+				function error() {
+					$scope.$emit('page:notFound');
+				}
+			);
 		};
 
 		$scope.wait = false;
