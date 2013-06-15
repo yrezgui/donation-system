f223e7c49cea952ca8f2d22243d603b725bf12d4
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-15 18:30:50 +0200

    Finish Stripe directive

--- app/scripts/directives/sprite-purchase.js
+++ app/scripts/directives/sprite-purchase.js
@@ -1,12 +1,32 @@
 'use strict';
 
 angular.module('floussApp')
-  .directive('spritePurchase', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the spritePurchase directive');
-      }
-    };
-  });
+	.directive('spritePurchase', ['config', function spritePurchase(config) {
+		return {
+			restrict: 'EAC',
+			scope: {
+				settings: '=spritePurchase',
+				token: '=',
+				callback: '&'
+			},
+			link: function ($scope, element, attrs) {
+				element.on('click', function click() {
+
+					StripeCheckout.open({
+						key: config.get('stripe_public_key'),
+						address: false,
+						amount: $scope.settings.price.toString().replace('.', ''),
+						currency: config.get('stripe_currency'),
+						name: $scope.settings.name,
+						description: $scope.settings.description,
+						panelLabel: config.get('stripe_button_label'),
+						token: function token(res) {
+							$scope.callback({token: res});
+						}
+					});
+
+					return false;
+				});
+			}
+		};
+	}]);
