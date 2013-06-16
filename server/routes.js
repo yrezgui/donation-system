1b691949e6ab448bd18eed76e1c8b1939160d4cf
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-17 00:54:58 +0200

    Remove useless API routes
    Add middleware of authentification

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,5 @@
+var clientModel	= require('./models/client.js');
+
 var client	= require('./controllers/api/client.js');
 var ebook	= require('./controllers/api/ebook.js');
 var invoice	= require('./controllers/api/invoice.js');
@@ -5,26 +7,32 @@ var invoice	= require('./controllers/api/invoice.js');
 // Application back end routing
 module.exports = function(parent){
 
+	// Middleware of authentification. Get automatically the logged client
+	parent.all('/api/*', function(req, res, next){
+		clientModel.findOne({token: req.query.token}, function(err, loggedClient){
+
+			if(!err && loggedClient) {
+				req.loggedClient = loggedClient;
+			}
+
+			next();
+		});
+	});
+
 	// Client entity API
-	parent.get('/api/client', client.query);
 	parent.post('/api/client', client.create);
 	parent.get('/api/client/:id', client.get);
 	parent.put('/api/client/:id', client.save);
-	parent.delete('/api/client/:id', client.remove);
 
 	parent.post('/api/client/login', client.login);
 
 	// Ebook entity API
 	parent.get('/api/ebook', ebook.query);
-	parent.post('/api/ebook', ebook.create);
 	parent.get('/api/ebook/:id', ebook.get);
-	parent.put('/api/ebook/:id', ebook.save);
-	parent.delete('/api/ebook/:id', ebook.remove);
 
 	// Invoice entity API
 	parent.get('/api/invoice', invoice.query);
 	parent.post('/api/invoice', invoice.create);
 	parent.get('/api/invoice/:id', invoice.get);
-	parent.put('/api/invoice/:id', invoice.save);
-	parent.delete('/api/invoice/:id', invoice.remove);
+	parent.get('/api/invoice/:id/download', invoice.download);
 };
