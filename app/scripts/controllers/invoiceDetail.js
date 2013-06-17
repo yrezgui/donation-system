3f3c50853c1043e8619212491e3d85261608be4f
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-17 11:02:56 +0200

    Finish invoiceDetail page

--- app/scripts/controllers/invoiceDetail.js
+++ app/scripts/controllers/invoiceDetail.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('floussApp')
-	.controller('InvoiceDetailCtrl', ['$scope', 'Restangular', '$routeParams', function InvoiceDetailCtrl($scope) {
-		$scope.invoice = Restangular.one('invoice', $routeParams.id);
+	.controller('InvoiceDetailCtrl', ['$scope', 'Restangular', '$routeParams', 'auth', function InvoiceDetailCtrl($scope, Restangular, $routeParams, auth) {
+		
+		$scope.auth = auth;
+
+		Restangular.one('invoice', $routeParams.id).get().then(
+			function success(resource) {
+				$scope.invoice = resource;
+			},
+
+			function error() {
+				$scope.$emit('page:notFound');
+			}
+		);
 	}]);
