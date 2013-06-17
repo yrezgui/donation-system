62a00d1b4f8a542a8b34f029b428264f95cf0023
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-17 13:03:02 +0200

    Finish ebookDetail page

--- app/scripts/controllers/ebookDetail.js
+++ app/scripts/controllers/ebookDetail.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('floussApp')
-	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', 'auth', 'flash', '$route', function EbookDetailCtrl($scope, Restangular, $routeParams, auth, flash, $route) {
+	.controller('EbookDetailCtrl', ['$scope', 'Restangular', '$routeParams', 'auth', 'flash', '$route', '$location', function EbookDetailCtrl($scope, Restangular, $routeParams, auth, flash, $route, $location) {
+
+		var safeApply = function safeApply(fn) {
+			($scope.$$phase || $scope.$root.$$phase) ? fn() : $scope.$apply(fn);
+		};
 
 		Restangular.one('ebook', $routeParams.id).get().then(
 			function success(resource) {
@@ -20,25 +24,48 @@ angular.module('floussApp')
 			}
 		);
 
+		Restangular.one('ebook', $routeParams.id).get().then(
+			function success(resource) {
+				$scope.ebook = resource;
+			},
+
+			function error() {
+				$scope.$emit('page:notFound');
+			}
+		);
+
 		$scope.makePayment = function makePayment(token) {
-			$scope.wait = true;
-			$scope.$digest();
+			safeApply(function() {
+				$scope.wait = true;
+			});
 
 			console.log(token);
 
 			var newInvoice = {
 				stripe_token: token.id,
 				client: auth.getId(),
-				file: $routeParams.id
+				ebook: $routeParams.id,
+				amount: $scope.ebook.price.toString().replace('.', '')
 			};
 
 			Restangular.all('invoice').post(newInvoice).then(
 				function success(resource) {
-					console.log("Object saved OK", resource);
+					console.log('Object saved OK', resource);
+
+					flash.add('payment-success', {
+						type: 'success',
+						title: 'Success !',
+						content: 'The payment success. You can now download the ebook by clicking the button in the bottom right.'
+					});
+
+					safeApply(function() {
+						$scope.wait = false;
+						$location.url('/invoice/' + resource._id);
+					});
 				},
 
 				function error() {
-					console.log("There was an error saving");
+					console.log('There was an error saving');
 
 					flash.add('payment-error', {
 						type: 'error',
@@ -46,28 +73,11 @@ angular.module('floussApp')
 						content: 'The payment failed. Please enter correct information and retry.'
 					});
 
-					$route.reload();
-				}
-			);
-
-
-			Restangular.one('ebook', $routeParams.id).get().then(
-				function success(resource) {
-
-					$scope.ebook = resource;
-
-					$scope.spriteSettings = {
-						price: $scope.ebook.price,
-						name: $scope.ebook.title,
-						description: $scope.ebook.description
-					};
-				},
-
-				function error() {
-					$scope.$emit('page:notFound');
+					safeApply(function() {
+						$scope.wait = true;
+						$route.reload();
+					});
 				}
 			);
 		};
-
-		$scope.wait = false;
 	}]);
